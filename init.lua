f8cbef7fcccc8df3f023ec7f6b745a703cbdf2c2
Lukasz B <llukasz3@gmail.com>
2025-10-20 00:48:14 +0200

    Added colorscheme

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+require 'core.options'
+require 'core.keymaps'
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -25,4 +28,22 @@ rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-})
+	  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+  }
+,
+{
+  'folke/tokyonight.nvim',
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  }})
+
+vim.cmd[[colorscheme tokyonight]]
