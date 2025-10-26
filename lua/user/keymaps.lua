8381278a057e3a2714f73e48d805ca102a901a43
Lukasz B <llukasz3@gmail.com>
2025-10-26 20:52:27 +0100

    Some changes to existing files

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -46,11 +46,11 @@ keymap("i", "jk", "<ESC>", opts)
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
-
 -- Move text up and down
 keymap("v", "<A-j>", ":m .+1<CR>==", opts)
 keymap("v", "<A-k>", ":m .-2<CR>==", opts)
 keymap("v", "p", '"_dP', opts)
+
 -- Visual Block --
 -- Move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
