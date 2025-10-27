8efd689d69d4d27f6312aa71be710955ec24407b
Lukasz B <llukasz3@gmail.com>
2025-10-27 23:30:24 +0100

    Added autocompletion

--- init.lua
+++ init.lua
@@ -32,7 +32,10 @@ require('lazy').setup({
   require 'plugins.colortheme',
   require 'plugins.bufferline',
   require 'plugins.lualine',
-  require 'plugins.treesitter'
+  require 'plugins.treesitter',
+  require 'plugins.telescope',
+  require 'plugins.lsp',
+  require 'plugins.autocompletion'
   })
 
 
